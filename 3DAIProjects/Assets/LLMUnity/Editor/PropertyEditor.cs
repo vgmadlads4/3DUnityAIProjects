d126217dc771d63b25d2f6f9e03323e1721009cc
vgmadlads4 <vgmadlads@gmail.com>
2024-07-25 22:08:37 -0400

    Downloaded the LLM

---
+++ 3DAIProjects/Assets/LLMUnity/Editor/PropertyEditor.cs
@@ -0,0 +1,171 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace LLMUnity
+{
+    public class PropertyEditor : Editor
+    {
+        protected int buttonWidth = 150;
+
+        public void AddScript(SerializedObject llmScriptSO)
+        {
+            var scriptProp = llmScriptSO.FindProperty("m_Script");
+            EditorGUILayout.PropertyField(scriptProp);
+        }
+
+        public void AddOptionsToggle(SerializedObject llmScriptSO, string propertyName, string name)
+        {
+            SerializedProperty advancedOptionsProp = llmScriptSO.FindProperty(propertyName);
+            string toggleText = (advancedOptionsProp.boolValue ? "Hide" : "Show") + " " + name;
+            GUIStyle style = new GUIStyle("Button");
+            if (advancedOptionsProp.boolValue)
+                style.normal = new GUIStyleState() { background = Texture2D.grayTexture };
+            if (GUILayout.Button(toggleText, style, GUILayout.Width(buttonWidth)))
+            {
+                advancedOptionsProp.boolValue = !advancedOptionsProp.boolValue;
+            }
+        }
+
+        public void AddSetupSettings(SerializedObject llmScriptSO)
+        {
+            List<Type> attributeClasses = new List<Type>(){typeof(LocalRemoteAttribute)};
+            attributeClasses.Add(llmScriptSO.FindProperty("remote").boolValue ? typeof(RemoteAttribute) : typeof(LocalAttribute));
+            attributeClasses.Add(typeof(LLMAttribute));
+            if (llmScriptSO.FindProperty("advancedOptions").boolValue)
+            {
+                attributeClasses.Add(typeof(LLMAdvancedAttribute));
+            }
+            ShowPropertiesOfClass("Setup Settings", llmScriptSO, attributeClasses, true);
+        }
+
+        public void AddChatSettings(SerializedObject llmScriptSO)
+        {
+            List<Type> attributeClasses = new List<Type>(){typeof(ChatAttribute)};
+            if (llmScriptSO.FindProperty("advancedOptions").boolValue)
+            {
+                attributeClasses.Add(typeof(ChatAdvancedAttribute));
+            }
+            ShowPropertiesOfClass("Chat Settings", llmScriptSO, attributeClasses, false);
+        }
+
+        public void AddOptionsToggles(SerializedObject llmScriptSO)
+        {
+            LLMUnitySetup.SetDebugMode((LLMUnitySetup.DebugModeType)EditorGUILayout.EnumPopup("Log Level", LLMUnitySetup.DebugMode));
+            EditorGUILayout.BeginHorizontal();
+            AddOptionsToggle(llmScriptSO, "advancedOptions", "Advanced Options");
+            EditorGUILayout.EndHorizontal();
+            Space();
+        }
+
+        public void Space()
+        {
+            EditorGUILayout.Space((int)EditorGUIUtility.singleLineHeight / 2);
+        }
+
+        protected virtual Type[] GetPropertyTypes()
+        {
+            return new Type[] {};
+        }
+
+        public List<SerializedProperty> GetPropertiesOfClass(SerializedObject so, List<Type> attributeClasses)
+        {
+            // display a property if it belongs to a certain class and/or has a specific attribute class
+            List<SerializedProperty> properties = new List<SerializedProperty>();
+            foreach (Type attrClass in attributeClasses)
+            {
+                if (attrClass == null) continue;
+                foreach (Type targetClass in GetPropertyTypes())
+                {
+                    SerializedProperty prop = so.GetIterator();
+                    if (prop.NextVisible(true))
+                    {
+                        do
+                        {
+                            if (PropertyInClass(prop, targetClass, attrClass))
+                                properties.Add(so.FindProperty(prop.propertyPath));
+                        }
+                        while (prop.NextVisible(false));
+                    }
+                }
+            }
+            return properties;
+        }
+
+        public void ShowPropertiesOfClass(string title, SerializedObject so, List<Type> attributeClasses, bool addSpace = true)
+        {
+            // display a property if it belongs to a certain class and/or has a specific attribute class
+            List<SerializedProperty> properties = GetPropertiesOfClass(so, attributeClasses);
+            if (properties.Count == 0) return;
+            if (title != "") EditorGUILayout.LabelField(title, EditorStyles.boldLabel);
+            foreach (SerializedProperty prop in properties)
+            {
+                Attribute floatAttr = GetPropertyAttribute(prop, typeof(FloatAttribute));
+                Attribute intAttr = GetPropertyAttribute(prop, typeof(IntAttribute));
+                if (floatAttr != null)
+                {
+                    EditorGUILayout.Slider(prop, ((FloatAttribute)floatAttr).Min, ((FloatAttribute)floatAttr).Max, new GUIContent(prop.displayName));
+                }
+                else if (intAttr != null)
+                {
+                    EditorGUILayout.IntSlider(prop, ((IntAttribute)intAttr).Min, ((IntAttribute)intAttr).Max, new GUIContent(prop.displayName));
+                }
+                else
+                {
+                    EditorGUILayout.PropertyField(prop);
+                }
+            }
+            if (addSpace) Space();
+        }
+
+        public bool PropertyInClass(SerializedProperty prop, Type targetClass, Type attributeClass = null)
+        {
+            // check if a property belongs to a certain class and/or has a specific attribute class
+            FieldInfo field = prop.serializedObject.targetObject.GetType().GetField(prop.name,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            return field != null && field.DeclaringType == targetClass && (attributeClass == null || GetPropertyAttribute(prop, attributeClass) != null);
+        }
+
+        public Attribute GetPropertyAttribute(SerializedProperty prop, Type attributeClass)
+        {
+            // check if a property has a specific attribute class
+            foreach (var pathSegment in prop.propertyPath.Split('.'))
+            {
+                var targetType = prop.serializedObject.targetObject.GetType();
+                while (targetType != null)
+                {
+                    var fieldInfo = targetType.GetField(pathSegment, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                    if (fieldInfo != null)
+                    {
+                        foreach (Attribute attr in fieldInfo.GetCustomAttributes(attributeClass, true))
+                        {
+                            if (attr.GetType() == attributeClass)
+                                return attr;
+                        }
+                    }
+                    targetType = targetType.BaseType;
+                }
+            }
+            return null;
+        }
+
+        public void OnInspectorGUIStart(SerializedObject scriptSO)
+        {
+            scriptSO.Update();
+            GUI.enabled = false;
+            AddScript(scriptSO);
+            GUI.enabled = true;
+            EditorGUI.BeginChangeCheck();
+        }
+
+        public void OnInspectorGUIEnd(SerializedObject scriptSO)
+        {
+            if (EditorGUI.EndChangeCheck())
+                Repaint();
+
+            scriptSO.ApplyModifiedProperties();
+        }
+    }
+}
